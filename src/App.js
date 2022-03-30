b908fa839e323ed2fea2bfb4b23778ca66a1d6c6
nabaraj <nabaraj2055@gmail.com>
2022-03-30 17:08:43 +0545

    look up registera page develop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Data } from "./tire.js";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("fetched data");
+    value.length > 0 && setData(Data);
+    setValue("");
+  };
+  const handleClear = () => {
+    setData([]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={value}
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Please,enter your car number"
+        />
+
+        <button>Quick Lookup</button>
+
+        <span onClick={handleClear}>Clear data</span>
+      </form>
+
+      <div className="details">
+        <ul>
+          {data.map((item, index) => {
+            return (
+              <li key={index}>
+                <div className="modal">
+                  <h1>Modal NO:</h1>
+                  <span>{item.Vehicle.ModelName}</span>
+                </div>
+                <li className="tire">
+                  <h1>Tire Details</h1>
+                  <div>
+                    <h2>Front</h2>
+                    <ul>
+                      <li>
+                        <span>Size:</span> <span>{item.Front.Tyre.Size}</span>
+                      </li>
+                      <li>
+                        <span>SectionWidth:</span>
+                        <span>{item.Front.Tyre.SectionWidth}</span>
+                      </li>
+                      <li>
+                        <span>AspectRatio:</span>
+                        <span>{item.Front.Tyre.AspectRatio}</span>
+                      </li>
+                      <li>
+                        {" "}
+                        <span>RimDiameter:</span>
+                        <span>{item.Front.Tyre.RimDiameter}</span>
+                      </li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h2>Rear</h2>
+                    <ul>
+                      <li>
+                        <span>Size:</span> <span>{item.Front.Tyre.Size}</span>
+                      </li>
+                      <li>
+                        <span>SectionWidth:</span>
+                        <span>{item.Front.Tyre.SectionWidth}</span>
+                      </li>
+                      <li>
+                        <span>AspectRatio:</span>
+                        <span>{item.Front.Tyre.AspectRatio}</span>
+                      </li>
+                      <li>
+                        {" "}
+                        <span>RimDiameter:</span>
+                        <span>{item.Front.Tyre.RimDiameter}</span>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/tire.js
@@ -0,0 +1,52 @@
+export const Data = [
+  {
+    Vehicle: {
+      ModelName: "Sharan. 2.0 TDi 170 SEL",
+      Power: {
+        Kw: "125",
+      },
+    },
+    Front: {
+      Tyre: {
+        Size: "225/50R17",
+        SectionWidth: 225,
+        AspectRatio: 50,
+        RimDiameter: 17,
+        LoadIndex: "98",
+        SpeedIndex: "V",
+        RunFlat: false,
+        Pressure: {
+          Bar: 2.7,
+          Psi: 39,
+        },
+      },
+      Rim: {
+        Size: "7 x 17",
+        Offset: "39",
+      },
+    },
+    Rear: {
+      Tyre: {
+        Size: "225/50R17",
+        SectionWidth: 225,
+        AspectRatio: 50,
+        RimDiameter: 17,
+        LoadIndex: "98",
+        SpeedIndex: "V",
+        RunFlat: false,
+        Pressure: {
+          Bar: 2.5,
+          Psi: 36,
+        },
+      },
+      Rim: {
+        Size: "7 x 17",
+        Offset: "39",
+      },
+    },
+    Hub: {
+      CenterBore: "57.1",
+      PCD: "5x112",
+    },
+  },
+];
