d9a2720601adc0987c12778c78d8219adaaab9ed
nabaraj <nabaraj2055@gmail.com>
2022-03-31 13:49:59 +0545

    Api intregration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Data } from "./tire.js";
 
 function App() {
   const [data, setData] = useState([]);
   const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+  const [hide, setHide] = useState("");
+  console.log("hellow", hide);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+  const fetchData = async () => {
+    try {
+      const datas = await axios.get(
+        `http://18.134.10.240:8082/tyre-data?registrationNumber=${value}`
+      );
+
+      console.log("jhellw", datas.data.body.Response);
+      datas.data.body.Response.StatusCode === "Success"
+        ? setData(datas.data.body.Response.DataItems.TyreDetails.RecordList)
+        : setError(datas.data.body.Response.StatusMessage);
+      setHide(datas.data.body.Response.StatusCode);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("fetched data");
-    value.length > 0 && setData(Data);
+    fetchData();
     setValue("");
   };
   const handleClear = () => {
@@ -30,6 +52,9 @@ function App() {
       </form>
 
       <div className="details">
+        <div>
+          <h3>{hide === "KeyInvalid" && error}</h3>
+        </div>
         <ul>
           {data.map((item, index) => {
             return (
